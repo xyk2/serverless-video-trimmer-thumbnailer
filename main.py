c7b57f48ab48a831ed847451e60509811a853628
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2018-12-18 01:48:48 +0800

    'fast' option to skip re-transcoding

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 from flask import Flask, abort, request, jsonify, send_file
 
-_allowed_params = ['s', 'f', 'q'] # Allowed parameters in the URL request
+_allowed_params = ['s', 'f', 'q', 'fast'] # Allowed parameters in the URL request
 SOURCE_BUCKET_NAME = 'LOL'
 DESTINATION_BUCKET_NAME = 'LOL'
 LOCAL_DESTINATION_PATH = './outputs'
@@ -33,15 +33,15 @@ def generate_hash(message):
 def request_signed_url(filename):
 	"Request and return a protected signed URL from the source bucket."
 
-	url = '{}/{}'.format('https://storage.googleapis.com/test_videos_mp4', urllib.parse.quote(filename))
+	url = '{}/{}'.format('http://storage.googleapis.com/test_videos_mp4', urllib.parse.quote(filename))
 	return url
 
-def check_in_memorystore(md5_hash):
-	"Check if this query is repeated in the memorystore."
+def check_in_datastore(md5_hash):
+	"Check if this unique query is repeated in the datastore."
 	return md5_hash
 
-def insert_to_memorystore(md5_hash, location):
-	"Add the query to the memorystore."
+def insert_to_datastore(md5_hash, location):
+	"Add this unique query to the datastore."
 	return (md5_hash, location)
 
 def upload_to_storage_and_return_url(filename):
@@ -63,8 +63,9 @@ def trim(request):
 	# Extract parameters from URL field
 	for param in _source_params.split(','):
 		try:
-			_key = param.split(':')[0]
-			_value = param.split(':')[1]
+			_split = param.split(':')
+			_key = _split[0]
+			_value = _split[1] if len(_split) > 1 else None
 
 			if _key in _allowed_params:
 				_params[_key] = _value
@@ -75,30 +76,40 @@ def trim(request):
 	_hash = generate_hash("{}:{}".format(json.dumps(_params, sort_keys=True), _source_file))
 
 	## Create args for ffmpeg.input()
+	_ffmpeg_input_args['multiple_requests'] = '1' # Reuse tcp connections
+
 	if 's' in _params and 'f' in _params:
 		_ffmpeg_input_args['ss'] = float(_params['s'])
 		_ffmpeg_input_args['t'] = float(_params['f']) - float(_params['s'])
 
-	## FFprobe
-	probe = ffmpeg.probe(request_signed_url(_source_file))
-	video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-	width = int(video_stream['width'])
-	height = int(video_stream['height'])
+	## FFprobe (requires another complete moov atom handshake, slow)
+	#probe = ffmpeg.probe(request_signed_url(_source_file))
+	#video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+	#width = int(video_stream['width'])
+	#height = int(video_stream['height'])
 
 	## Run FFMPEG
-	watermark = ffmpeg.input('./watermark.png')
+	#watermark = ffmpeg.input('./watermark.png')
 
 	job = ffmpeg.input(request_signed_url(_source_file), **_ffmpeg_input_args)
 	#job = ffmpeg.filter(job, 'fps', fps=25, round='up')
 	#job = ffmpeg.drawbox(job, 50, 50, 120, 120, color='red', thickness=5)
 	#job = ffmpeg.overlay(job, watermark)
-	job = ffmpeg.output(job, '{}/{}_{}.mp4'.format(LOCAL_DESTINATION_PATH, _time, _hash), **{'movflags': '+faststart', 'hide_banner': None, 'nostdin': None, 'c': 'copy'})
 
-	print(' '.join(ffmpeg.compile(job)))
+	kwargs = dict()
+	kwargs['movflags'] = '+faststart' # moov atom in front
+	kwargs['hide_banner'] = None # hide ffmpeg banner from logs
+	kwargs['nostdin'] = None # disable interactive mode
+	kwargs['loglevel'] = 'debug' # more detailed logs from ffmpeg
+	if 'fast' in _params: kwargs['c'] = 'copy'
+
+
+	job = ffmpeg.output(job, '{}/{}_{}.mp4'.format(LOCAL_DESTINATION_PATH, _time, _hash), **kwargs)
 
 	try:
 		err, out = ffmpeg.run(job, cmd=FFMPEG_BINARY_PATH, capture_stderr=True, capture_stdout=True)
 
+		# read source metadata from out?
 		#logging.info(out.decode('utf-8').replace('\n', ' '))
 		#logging.error(err.decode('utf-8').replace('\n', ' '))
 
@@ -110,18 +121,17 @@ def trim(request):
 		#print(e.stderr.decode(), file=sys.stderr)
 
 
-	return send_file('{}/{}_{}.mp4'.format(LOCAL_DESTINATION_PATH, _time, _hash))
+	#return send_file('{}/{}_{}.mp4'.format(LOCAL_DESTINATION_PATH, _time, _hash))
 
-	#return jsonify({
-	#	'params': _source_params,
-	#	'js_params': _params,
-	#	'ffmpeg_args': _ffmpeg_input_args,
-	#	'ffmpeg_command': " ".join(ffmpeg.compile(job)),
-	#	'source_file': _source_file,
-	#	# Generate unique hash based on sorted parameters + source filename
-	#	'hash': _hash,
-	#	'video_stream': video_stream
-	#})
+	return jsonify({
+		'params': _source_params,
+		'js_params': _params,
+		'ffmpeg_args': _ffmpeg_input_args,
+		'ffmpeg_command': " ".join(ffmpeg.compile(job)),
+		'source_file': _source_file,
+		'hash': _hash,
+		#'video_stream': video_stream
+	})
 
 # https://storage.googleapis.com/test_videos_mp4/%5B60fps%5D%205%20minutes%20timer%20with%20milliseconds-CW7-nFObkpw.mp4
 # https://storage.googleapis.com/test_videos_mp4/bigbuckbunny.mp4
@@ -160,28 +170,6 @@ if __name__=="__main__":
 	    return trim(request)
 
 	app.run('127.0.0.1', 5000, debug=True)
-
-
-
-
-
-"""
-def my_function(request):
-    return 'Hello World'
-
-if __name__ == "__main__":
-    from flask import Flask, request
-    app = Flask(__name__)
-
-    @app.route('/')
-    def index():
-        return my_function(request)
-
-    app.run('127.0.0.1', 8000, debug=True)
-"""
-
-
-
 
 
 
